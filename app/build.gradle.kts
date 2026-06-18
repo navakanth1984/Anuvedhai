dbaa70cd11c272d77e5e97e184a1be8bb9bd01c6
Navakanth Reddy Dumpa <navkanthr@gmail.com>
2026-06-18 18:51:33 +0530

    build: add gradle wrapper, fix namespace and project name for Play Store release
    - Add gradlew, gradlew.bat and gradle/wrapper/gradle-wrapper.properties (Gradle 8.11.1)
    - Fix namespace from com.example to com.aistudio.sarvamanuvad.xnkpqo
    - Rename rootProject from 'My Application' to 'Anuvedhai'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-  namespace = "com.example"
+  namespace = "com.aistudio.sarvamanuvad.xnkpqo"
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
